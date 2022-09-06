17fe7759867d94aeb8e7c10926e77825be5033df
dungli69 <90885190+dungli69@users.noreply.github.com>
2022-09-06 20:54:17 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
 import React from "react";
-import MercyshipContextProvider from "./context/MercyshipContext"
+
 
 function App() {
   return(
-    <ApolloProvider>
-    <App/>
-
-    </ApolloProvider>
-
+ <></>
   )
 }
 
