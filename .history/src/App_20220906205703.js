9b8ab0f09730cd9086b2e60d13e3a960bcdec736
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2022-09-06 20:58:12 +0700

    config-router(hoang)

---
+++ .history/src/App_20220906205703.js
@@ -0,0 +1,10 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/hooks/useErrorImg.js
@@ -0,0 +1,5 @@
+export default function onErrorImg(e) {
+  e.target.src = "";
+  e.target.style.objectFit = "contain";
+  //e.target.style.transform = "scale(0.5)";
+}
